c284898e27a8b610cf47f725f2130dfc61ca95d3
Tamas Marton <hello@tamasmarton.com>
2021-10-24 15:48:20 +0200

    Sign Up and Not Found page

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,32 +2,59 @@ import { useContext, useEffect, useState } from 'react'
 import { useHistory, Link } from 'react-router-dom'
 import FirebaseContext from '../context/firebase'
 import * as ROUTES from '../constants/routes'
+import { doesUsernameExist } from '../services/firebase'
 
-export default function Login() {
+export default function SignUp() {
   const history = useHistory()
   const { firebase } = useContext(FirebaseContext)
 
+  const [username, setUsername] = useState('')
+  const [fullName, setFullName] = useState('')
   const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('')
 
   const [error, setError] = useState('')
   const isInvalid = password === '' || emailAddress === ''
 
-  const handleLogin = async (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault()
 
-    try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password)
-      history.push(ROUTES.DASHBOARD)
-    } catch (error) {
-      setEmailAddress('')
-      setPassword('')
-      setError(error.message)
+    const usernameExists = await doesUsernameExist(username)
+
+    if (!usernameExists.length) {
+      try {
+        const createdUserResult = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(emailAddress, password)
+
+        await createdUserResult.user.updateProfile({
+          displayName: username
+        })
+
+        await firebase.firestore().collection('users').add({
+          userId: createdUserResult.user.uid,
+          username: username.toLowerCase(),
+          fullName,
+          emailAddress: emailAddress.toLowerCase(),
+          following: [],
+          dateCreated: Date.now()
+        })
+
+        history.push(ROUTES.DASHBOARD)
+      } catch (error) {
+        setFullName('')
+        setEmailAddress('')
+        setPassword('')
+        setError(error.message)
+      }
+    } else {
+      setUsername('')
+      setError('That username is already taken, please try another.')
     }
   }
 
   useEffect(() => {
-    document.title = 'Login Instagram'
+    document.title = 'Sign Up for Instagram'
   }, [])
 
   return (
@@ -44,7 +71,25 @@ export default function Login() {
 
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignUp} method="POST">
+            <input
+              type="text"
+              placeholder="Username"
+              aria-label="Enter your username"
+              className="text-sm text-gray-base w-full h-2 py-5 px-4 mr-3 mb-2 border border-gray-primary rounded"
+              onChange={({ target }) => setUsername(target.value)}
+              value={username}
+            />
+
+            <input
+              type="text"
+              placeholder="Full Name"
+              aria-label="Enter your full name"
+              className="text-sm text-gray-base w-full h-2 py-5 px-4 mr-3 mb-2 border border-gray-primary rounded"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
+            />
+
             <input
               type="email"
               placeholder="Email address"
@@ -70,16 +115,16 @@ export default function Login() {
                 ${isInvalid && 'opacity-50'}
               `}
             >
-              Log In
+              Sign Up
             </button>
           </form>
         </div>
 
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
           <p className="tect-sm">
-            Don't have an account?{` `}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign up
+            Have an account?{` `}
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+              Log In
             </Link>
           </p>
         </div>
