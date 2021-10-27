7b13ba497e18865988907d9ac774a644f947fb21
Tamas Marton <hello@tamasmarton.com>
2021-10-27 14:22:20 +0200

    Timeline functionality
    - Post
    - Comments
    - Likes
    - etc.

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,26 @@
+/* eslint-disable no-nested-ternary */
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import usePhotos from '../hooks/use-photos'
+import Post from './post'
+
 export default function Timeline() {
+  // get logged in user's photos
+  // on loading use React-Skeleton
+  // if loaded, render photos (in post component)
+  // if user has no photos, tell them
+
+  const { photos } = usePhotos()
+
   return (
     <div className="container col-span-2">
-      <p>timeline</p>
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
     </div>
   )
 }
